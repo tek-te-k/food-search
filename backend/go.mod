bf092bc65d7e2375e9f6d23efe7d777147db72dc
kenkn <skt7tp@gmail.com>
2022-08-30 20:39:37 +0900

    :sparkles: Create NearbySearch

--- backend/go.mod
+++ backend/go.mod
@@ -3,8 +3,11 @@ module food-search-backend
 go 1.16
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible
+	github.com/go-playground/validator/v10 v10.11.0
+	github.com/google/go-cmp v0.4.0 // indirect
 	github.com/labstack/echo/v4 v4.8.0
+	github.com/pkg/errors v0.9.1 // indirect
+	googlemaps.github.io/maps v1.3.2
 	gorm.io/driver/postgres v1.3.9
 	gorm.io/gorm v1.23.8
 )
--- backend/routers/api/v1/food.go
+++ backend/routers/api/v1/food.go
@@ -1,7 +1,43 @@
 package v1
 
-import "github.com/labstack/echo/v4"
+import (
+	"context"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"googlemaps.github.io/maps"
+)
+
+const nearbySearchRadius = 2000
+
+type SearchFoodsRequest struct {
+	Latitude  float64 `json:"latitude" validate:"required"`
+	Longitude float64 `json:"longitude" validate:"required"`
+	Keyword   string  `json:"keyword" validate:"required"`
+}
 
 func SearchFoods(c echo.Context) error {
-	return c.String(200, "Hello, World!")
+	apiKey := os.Getenv("GOOGLE_API_KEY")
+	if apiKey == "" {
+		return echo.NewHTTPError(http.StatusInternalServerError, "FOOD_API_KEY is not set")
+	}
+	req := new(SearchFoodsRequest)
+	if err := c.Bind(req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	client, err := maps.NewClient(maps.WithAPIKey(apiKey))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	pr, err := client.NearbySearch(context.Background(), &maps.NearbySearchRequest{
+		Location: &maps.LatLng{
+			Lat: req.Latitude,
+			Lng: req.Longitude,
+		},
+		Radius:  nearbySearchRadius,
+		Keyword: req.Keyword,
+	})
+	return c.JSON(200, pr)
 }
